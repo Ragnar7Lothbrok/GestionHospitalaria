cc909c21c9e1d5ad025d66d4bf421634aeedb4ea
Ragnar7Lothbrok <kiqqe.7@gmail.com>
2025-03-24 22:32:55 +0100

    Añadidos los archivos README.md para cada tarea

--- Ejercicio1/Tarea3/obj/Debug/net9.0/Tarea3.AssemblyInfo.cs
+++ Ejercicio1/Tarea3/obj/Debug/net9.0/Tarea3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364ee71515c6181f6383c35b24aac8fd1255d59b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af22c6f6f67504a61e90c859c7a5068a01c9e036")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
